843f10947959f391e115a4551ed1a3edc6d8d674
Mitul Sarvaiya <mitulsarvaiya9500@gmail.com>
2018-09-09 20:27:11 +0530

    Created file for displaying data in Pagination

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>  
+ <html>  
+      <head>  
+           <title>Pagination using Jquery, PHP, Ajax and MySQL</title>  
+           <script src="js/jquery.min.js"></script>  
+           <script src="js/jquery-3.3.1.js"></script>  
+           <script src="js/main.js"></script>  
+      </head>  
+      <body>  
+           <br /><br />  
+           <div class="container">  
+                <h3 align="center">Dashboard</h3><br />
+                <div id="pagination_data" align="center">  
+                </div>  
+           </div>  
+      </body>  
+ </html>  
